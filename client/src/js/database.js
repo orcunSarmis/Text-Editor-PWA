12ccd6021b5c10f9a9402ca3ae3e18a76faf378d
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-22 18:58:47 +1000

    working on database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+   console.error('putDb not implemented');
+
+// Create a connection to the database database and version we want to use.
+const jateDB = await openDB('jate', 1);
+
+// Create a new transaction and specify the database and data privileges.
+const tx = jateDB.transaction('jate', 'readwrite');
+
+// Open up the desired object store.
+const store = tx.objectStore('jate');
+
+// Use the .put() method on the store and pass in the content.
+const request = store.put({ constent: content });
+
+ // Get confirmation of the request.
+const result = await request;
+console.log('data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
