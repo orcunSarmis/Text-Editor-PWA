d5d98bfa223e44ff14abf86cdbb97884d5f597fb
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-24 22:01:37 +1000

    working on error

--- client/src-sw.js
+++ client/src-sw.js
@@ -29,7 +29,6 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({ 
     // name of the cache storage
