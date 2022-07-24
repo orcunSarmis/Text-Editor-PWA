1ef694b5f69033b6d0ae03bc58bf2cf050960a18
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-24 17:38:59 +1000

    try to fixing error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   console.log('Post to the jate');
 
 // Create a connection to the database database and version we want to use.
@@ -26,7 +26,7 @@ const tx = jateDB.transaction('jate', 'readwrite');
 const store = tx.objectStore('jate');
 
 // Use the .put() method on the store and pass in the content.
-const request = store.put({ id:1, value: content });
+const request = store.put({ id: id, value: content });
 
  // Get confirmation of the request.
 const result = await request;
